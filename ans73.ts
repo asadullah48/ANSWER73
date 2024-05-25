7cbfc0b8a950f7ef7199f19334155ed7c42241ff
asadullah48 <asadullahshafique@hotmail.com>
2024-05-25 11:27:51 +0500

    Initial commit

---
+++ ans73.ts
@@ -0,0 +1,12 @@
+// Q73
+// This function shows how to assign and update variable values
+function updateVariable() {
+    let number = 69; // Initially assigns the value 69
+    console.log("Initial value:", number); // Logs the initial value
+    number = 138; // Updates the value to 138
+    console.log("Updated value:", number); // Logs the updated value
+  }
+  
+  updateVariable();
+  // We assign a value to a variable and then 
+  //update it, showing how values can change.
